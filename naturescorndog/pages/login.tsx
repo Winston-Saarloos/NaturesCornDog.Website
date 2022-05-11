53efca501969ed80c8d9c661b85eec200a05f366
97rocko <97rocko@gmail.com>
2022-05-11 03:36:12 -0500

    styled the auth pages

--- naturescorndog/pages/login.tsx
+++ naturescorndog/pages/login.tsx
@@ -2,64 +2,80 @@ import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Paper from "@mui/material/Paper";
 import { useAuth } from '../context/AuthContext';
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
   const router = useRouter()
   const { user, login } = useAuth()
   const [data, setData] = useState({
-    email: '',
-    password: '',
-  })
+    email: "",
+    password: "",
+  });
 
   const handleLogin = async () => {
     console.log("Called login");
 
-    console.log(user)
+    console.log(user);
     try {
       await login(data.email, data.password);
-      router.push('/dashboard');
+      router.push("/dashboard");
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
-    <div
-      style={{
-        width: "40%",
-        margin: "auto",
-      }}
-    >
-      <h1>Login</h1>
-      <TextField
-        id="txtEmail"
-        label="Email"
-        variant="outlined"
-        onChange={(e: any) =>
-          setData({
-            ...data,
-            email: e.target.value,
-          })
-        }
-        value={data.email}
-        type="email"
-      />
+    <div className={styles.main}>
+      <div>
+        <Paper elevation={3} className={styles.container}>
+          <div>
+            <h1>Login</h1>
+          </div>
+          <div className={styles.elementContainer}>
+            <TextField
+              id="txtEmail"
+              label="Email"
+              variant="outlined"
+              onChange={(e: any) =>
+                setData({
+                  ...data,
+                  email: e.target.value,
+                })
+              }
+              value={data.email}
+              type="email"
+              className={styles.textField}
+            />
+          </div>
 
-      <TextField
-        id="txtPassword"
-        type="password"
-        label="Password"
-        variant="outlined"
-        onChange={(e: any) =>
-          setData({
-            ...data,
-            password: e.target.value,
-          })
-        }
-        value={data.password}
-      />
-      <Button variant="outlined" onClick={() => handleLogin()}>Login</Button>
+          <div className={styles.elementContainer}>
+            <TextField
+              id="txtPassword"
+              type="password"
+              label="Password"
+              variant="outlined"
+              onChange={(e: any) =>
+                setData({
+                  ...data,
+                  password: e.target.value,
+                })
+              }
+              value={data.password}
+              className={styles.textField}
+            />
+          </div>
+          <div className={styles.elementContainer}>
+              Don't have an account already? Sign Up
+          </div>
+          <div>
+            <Button variant="outlined" onClick={() => handleLogin()}>
+              Login
+            </Button>
+          </div>
+        </Paper>
+      </div>
     </div>
   );
 }
--- naturescorndog/pages/signup.tsx
+++ naturescorndog/pages/signup.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { useAuth } from '../context/AuthContext'
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import { useAuth } from "../context/AuthContext";
+import styles from "../styles/SignUp.module.css";
 
 const Signup = () => {
   const { user, signup } = useAuth();
   console.log(user);
+
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -22,44 +25,55 @@ const Signup = () => {
   };
 
   return (
-    <div
-      style={{
-        width: "40%",
-        margin: "auto",
-      }}
-    >
-      <h1>Signup</h1>
-      <TextField
-        id="txtEmail"
-        label="Email"
-        variant="outlined"
-        onChange={(e: any) =>
-          setData({
-            ...data,
-            email: e.target.value,
-          })
-        }
-        value={data.email}
-        type="email"
-      />
+    <div className={styles.main}>
+      <div>
+        <Paper elevation={3} className={styles.container}>
+          <div>
+            <h1>Signup</h1>
+          </div>
+          <div className={styles.elementContainer}>
+            <TextField
+              id="txtEmail"
+              label="Email"
+              variant="outlined"
+              onChange={(e: any) =>
+                setData({
+                  ...data,
+                  email: e.target.value,
+                })
+              }
+              value={data.email}
+              type="email"
+              className={styles.textField}
+            />
+          </div>
 
-      <TextField
-        id="txtPassword"
-        type="password"
-        label="Password"
-        variant="outlined"
-        onChange={(e: any) =>
-          setData({
-            ...data,
-            password: e.target.value,
-          })
-        }
-        value={data.password}
-      />
+          <div className={styles.elementContainer}>
+            <TextField
+              id="txtPassword"
+              type="password"
+              label="Password"
+              variant="outlined"
+              onChange={(e: any) =>
+                setData({
+                  ...data,
+                  password: e.target.value,
+                })
+              }
+              value={data.password}
+              className={styles.textField}
+            />
+          </div>
 
-      <Button variant="outlined" onClick={() => handleSignup()}>Sign Up</Button>
+          <div>
+            <Button variant="outlined" onClick={() => handleSignup()}>
+              Sign Up
+            </Button>
+          </div>
+        </Paper>
+      </div>
     </div>
   );
 };
 
-export default Signup
+export default Signup;
